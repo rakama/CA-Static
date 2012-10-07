6a361b7772f1f398291ff92874cf5aa3bbeef701
RamsesA <ramsesakama@gmail.com>
2012-10-07 07:16:00 -0400

    Added notice

--- src/com/github/rakama/plugins/cas/CAStatic.java
+++ src/com/github/rakama/plugins/cas/CAStatic.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2012, RamsesA <ramsesakama@gmail.com>
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+ * REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+ * AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+ * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+ * OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ */
+
 package com.github.rakama.plugins.cas;
 
 import java.util.HashSet;
@@ -16,6 +32,7 @@ import org.bukkit.event.block.BlockGrowEvent;
 import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.event.block.BlockPhysicsEvent;
 import org.bukkit.event.block.BlockSpreadEvent;
+import org.bukkit.event.server.MapInitializeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CAStatic extends JavaPlugin implements Listener
@@ -56,10 +73,19 @@ public class CAStatic extends JavaPlugin implements Listener
 
         for(String name : worldNames)
         {
-            World w = getServer().getWorld(name);
-            if(w != null)
-                worlds.add(w.getUID());
+            World world = getServer().getWorld(name);
+            if(world != null)
+                worlds.add(world.getUID());
         }
+    }
+
+    @EventHandler
+    public void onMapInitialize(MapInitializeEvent event)
+    {   
+        World world = event.getMap().getWorld();
+        String name = world.getName().toLowerCase();
+        if(worldNames.contains(name))
+            worlds.add(world.getUID());
     }
 
     protected boolean processEvent(BlockEvent e)
