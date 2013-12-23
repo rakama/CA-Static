0f7545d1f882f054bf5b3094dbc4f868264bfb9e
Rumsey <ramsesakama@gmail.com>
2013-12-23 01:28:53 -0500

    Updating for 1.0

--- src/org/craftingazeroth/plugins/cas/CAStatic.java
+++ src/org/craftingazeroth/plugins/cas/CAStatic.java
@@ -14,17 +14,15 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-package com.github.rakama.plugins.cas;
+package org.craftingazeroth.plugins.cas;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.UUID;
-
-import org.bukkit.World;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockBurnEvent;
-import org.bukkit.event.block.BlockEvent;
 import org.bukkit.event.block.BlockFadeEvent;
 import org.bukkit.event.block.BlockFormEvent;
 import org.bukkit.event.block.BlockFromToEvent;
@@ -37,70 +35,34 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class CAStatic extends JavaPlugin implements Listener
 {
+    // TODO: fix redstone
+    
     boolean enabled;
-    Set<String> worldNames;
-    Set<UUID> worlds;
 
     public CAStatic()
     {
         enabled = true;
-
-        worlds = new HashSet<UUID>();
-        worldNames = new HashSet<String>();
-
-        worldNames.add("kalimdor");
-        worldNames.add("kalimdor+001");
-        worldNames.add("kalimdor+002");
-        worldNames.add("kalimdor+003");
-        worldNames.add("kalimdor+004");
-        worldNames.add("kalimdor+005");
-        worldNames.add("kalimdor-001");
-
-        worldNames.add("azeroth");
-        worldNames.add("azeroth+001");
-        worldNames.add("azeroth+002");
-        worldNames.add("azeroth-001");
     }
 
     public void onDisable()
     {
-        worlds.clear();
     }
 
     public void onEnable()
     {
         getServer().getPluginManager().registerEvents(this, this);
-
-        for(String name : worldNames)
-        {
-            World world = getServer().getWorld(name);
-            if(world != null)
-                worlds.add(world.getUID());
-        }
     }
 
     @EventHandler
     public void onMapInitialize(MapInitializeEvent event)
     {   
-        World world = event.getMap().getWorld();
-        String name = world.getName().toLowerCase();
-        if(worldNames.contains(name))
-            worlds.add(world.getUID());
-    }
-
-    protected boolean processEvent(BlockEvent e)
-    {
-        if(!enabled)
-            return false;
-
-        return worlds.contains(e.getBlock().getWorld().getUID());
     }
 
     @EventHandler
     public void onBlockPhysics(BlockPhysicsEvent event)
     {
         // stop sand and gravel from falling
-        if(processEvent(event))
+        if(enabled)
             event.setCancelled(true);
     }
 
@@ -108,7 +70,7 @@ public class CAStatic extends JavaPlugin implements Listener
     public void onBlockBurn(BlockBurnEvent event)
     {
         // stop fire from destroying blocks
-        if(processEvent(event))
+        if(enabled)
             event.setCancelled(true);
     }
 
@@ -116,7 +78,7 @@ public class CAStatic extends JavaPlugin implements Listener
     public void onBlockIgnite(BlockIgniteEvent event)
     {
         // stop fire from spreading or igniting naturally
-        if(processEvent(event) 
+        if(enabled 
         &&(event.getCause() == BlockIgniteEvent.IgniteCause.SPREAD
         || event.getCause() == BlockIgniteEvent.IgniteCause.LAVA 
         || event.getCause() == BlockIgniteEvent.IgniteCause.LIGHTNING))
@@ -127,7 +89,7 @@ public class CAStatic extends JavaPlugin implements Listener
     public void onBlockFromTo(BlockFromToEvent event)
     {
         // stop liquids from flowing
-        if(processEvent(event))
+        if(enabled)
             event.setCancelled(true);
     }
 
@@ -135,24 +97,27 @@ public class CAStatic extends JavaPlugin implements Listener
     public void onBlockFade(BlockFadeEvent event)
     {
         // stop ice and snow from melting
-        if(processEvent(event))
+        if(enabled)
             event.setCancelled(true);
     }
 
     @EventHandler
     public void onBlockGrow(BlockGrowEvent event)
     {
-        // stop cacti (81) from growing
-        if(processEvent(event))
-            if(event.getNewState().getBlock().getTypeId() == 81)
+        // stop cacti and vines from growing
+        if(enabled)
+        {
+            int id = event.getNewState().getBlock().getTypeId();     
+            if(id == 81 || id == 106)
                 event.setCancelled(true);
+        }
     }
 
     @EventHandler
     public void onBlockForm(BlockFormEvent event)
     {
         // stop snow and ice from forming
-        if(processEvent(event))
+        if(enabled)
             event.setCancelled(true);
     }
 
@@ -160,7 +125,27 @@ public class CAStatic extends JavaPlugin implements Listener
     public void onBlockSpread(BlockSpreadEvent event)
     {
         // stop fire and mushrooms from spreading
-        if(processEvent(event))
+        if(enabled)
             event.setCancelled(true);
     }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event)
+    {
+        Block block = event.getBlock();
+        if(block.getTypeId() != 123)
+            return;
+        
+        event.setCancelled(true);
+        
+        Location below = block.getLocation().subtract(0, 1, 0);        
+        Block temp = block.getWorld().getBlockAt(below);
+        
+        int tempid = temp.getTypeId();
+        byte tempdata = temp.getData();
+        
+        temp.setTypeId(152, false);
+        block.setTypeId(124, false);
+        temp.setTypeIdAndData(tempid, tempdata, false);
+    }   
 }
